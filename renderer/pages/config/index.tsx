6502214d0c4022ba6b0f9887def7b5df788289d8
CarmenGitIt <carmenwhitton@gmail.com>
2025-02-18 01:16:20 -0500

    feat: edit config

--- renderer/pages/config/index.tsx
+++ renderer/pages/config/index.tsx
@@ -1,20 +1,30 @@
-import { Typography, Grid, Stack, FormControl, InputLabel, Select, MenuItem, SelectChangeEvent, Box, List, ListItem, ListItemText, Paper, Button } from "@mui/material"
+import { Typography, Grid, Stack, FormControl, InputLabel, Select, MenuItem, SelectChangeEvent, Box, List, ListItem, ListItemText, Paper, Button, TextField, Card, CardContent, CardHeader } from "@mui/material"
 import { useAtom } from "jotai"
 import React, { useEffect, useState } from "react"
-import { currentPlaylistConfigIdAtom, filesAtom, playlistConfigAtom } from "../../store/store"
+import { currentPlaylistConfigIdAtom, filesAtom, playlistConfigAtom, typesAtom } from "../../store/store"
 
 export default function Config() {
     const [configs, setConfigs] = useAtom(playlistConfigAtom);
     const [currentConfig, setCurrentConfig] = useAtom(currentPlaylistConfigIdAtom);
-
     const [files, setFiles] = useAtom(filesAtom);
+    const [types, setTypes] = useAtom(typesAtom);
 
-    // const [hasWindow, setHasWindow] = useState(false);
-    // useEffect(() => {
-    //     if (typeof window !== "undefined") {
-    //         setHasWindow(true);
-    //     }
-    // }, []);
+    const [startMinutes, setStartMinutes] = useState(new Array(100));
+    const [startHours, setStartHours] = useState(new Array(100));
+    const [endMinutes, setEndMinutes] = useState(new Array(100));
+    const [endHours, setEndHours] = useState(new Array(100));
+    const [name, setName] = useState(configs[currentConfig].name);
+    const [description, setDescription] = useState(configs[currentConfig].description);
+
+    const [hasWindow, setHasWindow] = useState(false);
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            setHasWindow(true);
+        }
+    }, []);
+
+    const minutesArr = Array.from({length: 60}, (x, i) => i);
+    const hoursArr = Array.from({length: 24}, (x, i) => i);
 
     function handleChangeCurrentConfig(e: SelectChangeEvent) {
         setCurrentConfig(Number.parseInt(e.target.value));
@@ -32,6 +42,86 @@ export default function Config() {
         // TODO: Add folder and all subfolders, use folder names as tags
     }
 
+    function handleSaveConfig() {
+        var tempConfigs = configs.slice();
+        for (var i = 0; i < tempConfigs[currentConfig].slots.length; i++) {
+            // Start time
+            console.log(startMinutes[i] + ": " + startHours[i]);
+            if (startMinutes[i] !== undefined && startHours[i] !== undefined) {
+                console.log('A');
+                tempConfigs[currentConfig].slots[i].startTime = startMinutes[i] + startHours[i] * 60; // TODO: Deal with unchanged values
+                console.log(tempConfigs[currentConfig].slots[i].startTime);
+            }
+            else if (startMinutes[i] !== undefined) {
+                console.log('B');
+                tempConfigs[currentConfig].slots[i].startTime = tempConfigs[currentConfig].slots[i].startTime % 60 + startMinutes[i];
+            }
+            else if (startHours[i] !== undefined) {
+                console.log('C');
+                tempConfigs[currentConfig].slots[i].startTime = Math.floor(tempConfigs[currentConfig].slots[i].startTime / 60) + startHours[i] * 60
+            }
+            console.log('D');
+            // End time
+            if (endMinutes[i] !== undefined && endHours[i] !== undefined) {
+                console.log('E');
+                tempConfigs[currentConfig].slots[i].endTime = endMinutes[i] + endHours[i] * 60; // TODO: Deal with unchanged values
+            }
+            else if (endMinutes[i] !== undefined) {
+                console.log('F');
+                tempConfigs[currentConfig].slots[i].endTime = tempConfigs[currentConfig].slots[i].endTime % 60 + endMinutes[i];
+            }
+            else if (endHours[i] !== undefined) {
+                console.log('G');
+                tempConfigs[currentConfig].slots[i].endTime = Math.floor(tempConfigs[currentConfig].slots[i].endTime / 60) + endHours[i] * 60
+            }
+            
+            // TODO: assign type, muted, volume
+        }
+        tempConfigs[currentConfig].name = name;
+        tempConfigs[currentConfig].description = description;
+        setConfigs(tempConfigs);
+    }
+
+
+    function handleChangeStartHour(e: SelectChangeEvent<Number>) {
+        var index = Number.parseInt(e.target.name);
+        var tempHours = startHours.slice();
+        tempHours[index] = e.target.value;                 
+        setStartHours(tempHours);
+    }
+
+    function handleChangeStartMinute(e: SelectChangeEvent<Number>) {
+        var index = Number.parseInt(e.target.name);
+        var tempMinutes = startMinutes.slice();                 
+        tempMinutes[index] = e.target.value;
+        setStartMinutes(tempMinutes);
+    }
+
+    function handleChangeEndHour(e: SelectChangeEvent<Number>) {
+        var index = Number.parseInt(e.target.name);
+        var tempHours = endHours.slice();
+        tempHours[index] = e.target.value;                     
+        setEndHours(tempHours);
+    }
+
+    function handleChangeEndMinute(e: SelectChangeEvent<Number>) {
+        var index = Number.parseInt(e.target.name);
+        var tempMinutes = endMinutes.slice();          
+        tempMinutes[index] = e.target.value;
+        setEndMinutes(tempMinutes);
+    }
+
+
+    function handleChangeName(e) {
+        setName(e.target.value);
+    }
+
+    function handleChangeDescription(e) {
+        setDescription(e.target.value);
+    }
+    
+    // TODO: Do the same as above but for slot type
+
     useEffect(() => {
 
     }, [configs, currentConfig]);
@@ -45,7 +135,7 @@ export default function Config() {
     return (
         <React.Fragment>
             <Typography variant="h1" gutterBottom>Config</Typography>
-            <Grid container spacing={2}>
+            <Grid container spacing={2} sx={{paddingBottom: 1}}>
                 <Grid item md={6}>
                     <Paper elevation={3} sx={{padding: 2}}>
                         <Stack
@@ -61,18 +151,84 @@ export default function Config() {
                                         {configs.map((config) => <MenuItem key={config.id} value={config.id}>{config.name}</MenuItem>) }
                                     </Select>
                             </FormControl>
-                            <Box sx={{borderBottom: 1, borderColor: 'divider', marginBottom: 3, maxHeight: 400, overflow: 'auto'}}>
+                            <Box sx={{borderBottom: 1, borderColor: 'divider', marginBottom: 3, maxHeight: 400, paddingTop: 1, paddingRight: 2, overflow: 'auto'}}>
                                 <Stack
                                     spacing={2}>
-                                    <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
-                                        {Object.values(configs[currentConfig]).map((value, key) => 
-                                            <ListItem
-                                            key={(key + 2) * 50}
-                                            >
-                                                <ListItemText primary={Object.keys(configs[currentConfig])[key] + ": " + value} />
-                                            </ListItem>
+                                    <TextField id="config-name" label="Name" variant="outlined" defaultValue={configs[currentConfig].name} onChange={handleChangeName}/>
+                                    <TextField id="config-description" label="Description" variant="outlined" defaultValue={configs[currentConfig].description} multiline onChange={handleChangeDescription}/>
+                                    <Paper elevation={6}>
+                                        {configs[currentConfig].slots.map((value, key) => 
+                                            <Card variant="outlined" key={key + 1200} sx={{marginTop: 1}}>
+                                                <CardHeader title={"Slot " + key}/>
+                                                <CardContent>
+                                                    <FormControl fullWidth>
+                                                        <InputLabel id={"slot-type-select-label" + key}>Media Type</InputLabel>
+                                                        <Select
+                                                            labelId={"slot-type-select-label" + key}
+                                                            id={"slot-type-select" + key}
+                                                            defaultValue={value.type.id}
+                                                            label="Media Type">
+                                                                {types.map((type) => <MenuItem key={type.id + 800} value={type.id}>{type.name}</MenuItem>) }
+                                                        </Select>
+                                                    </FormControl>
+                                                    <Typography>Start Time</Typography>
+                                                    <Stack direction="row" spacing={2} sx={{paddingTop: 1}}>
+                                                        <FormControl fullWidth>
+                                                            <InputLabel id={"slot-hour-select-label" + key}>Hour</InputLabel>
+                                                            <Select
+                                                                labelId={"slot-hour-select-label" + key}
+                                                                id={"slot-hour-select" + key}
+                                                                name={key + ""}
+                                                                defaultValue={Math.floor(value.startTime / 60)}
+                                                                onChange={handleChangeStartHour}
+                                                                label="Hour">
+                                                                    {hoursArr.map((val) => <MenuItem key={val + 1300} value={val}>{val}</MenuItem>)}
+                                                            </Select>
+                                                        </FormControl>
+                                                        <FormControl fullWidth >
+                                                            <InputLabel id={"slot-minute-select-label" + key}>Minute</InputLabel>
+                                                            <Select
+                                                                labelId={"slot-minute-select-label" + key}
+                                                                id={"slot-minute-select" + key}
+                                                                name={key + ""}
+                                                                defaultValue={value.startTime % 60}
+                                                                onChange={handleChangeStartMinute}
+                                                                label="Minute">
+                                                                    {minutesArr.map((val) => <MenuItem key={val + 1400} value={val}>{val}</MenuItem>)}
+                                                            </Select>
+                                                        </FormControl>
+                                                    </Stack>
+                                                    <Typography>End Time</Typography>
+                                                    <Stack direction="row" spacing={2} sx={{paddingTop: 1}}>
+                                                        <FormControl fullWidth>
+                                                            <InputLabel id={"slot-end-hour-select-label" + key}>Hour</InputLabel>
+                                                            <Select
+                                                                labelId={"slot-end-hour-select-label" + key}
+                                                                id={"slot-end-hour-select" + key}
+                                                                name={key + ""}
+                                                                defaultValue={Math.floor(value.endTime / 60)}
+                                                                onChange={handleChangeEndHour}
+                                                                label="Hour">
+                                                                    {hoursArr.map((val) => <MenuItem key={val + 1300} value={val}>{val}</MenuItem>)}
+                                                            </Select>
+                                                        </FormControl>
+                                                        <FormControl fullWidth >
+                                                            <InputLabel id={"slot-end-minute-select-label" + key}>Minute</InputLabel>
+                                                            <Select
+                                                                labelId={"slot-end-minute-select-label" + key}
+                                                                id={"slot-end-minute-select" + key}
+                                                                name={key + ""}
+                                                                defaultValue={value.endTime % 60}
+                                                                onChange={handleChangeEndMinute}
+                                                                label="Minute">
+                                                                    {minutesArr.map((val) => <MenuItem key={val + 1400} value={val}>{val}</MenuItem>)}
+                                                            </Select>
+                                                        </FormControl>
+                                                    </Stack>
+                                                </CardContent>
+                                            </Card>
                                         )}
-                                    </List>
+                                    </Paper>
                                     <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
                                         {configs[currentConfig].slots.map((value, key) => 
                                             <ListItem
@@ -82,6 +238,7 @@ export default function Config() {
                                             </ListItem>
                                         )}
                                     </List>
+                                    <Button onClick={handleSaveConfig} variant="outlined" color="success" sx={{width:"100%"}}>Save</Button>
                                 </Stack>
                             </Box>
                         </Stack>
@@ -111,10 +268,8 @@ export default function Config() {
                             </Paper>
                         </Stack>
                     </Paper>
-
                 </Grid>
             </Grid>
-            
         </React.Fragment>
     )
 }
--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -16,7 +16,7 @@ import { useState, useEffect } from 'react'
 import Config from './config'
 import { useSetAtom } from 'jotai'
 
-import {filesAtom, playlistPlayerAtom} from '../store/store';
+import {filesAtom, playlistPlayerAtom, typesAtom} from '../store/store';
 import { playlistConfigAtom } from '../store/store'
 import VideoFile from '../lib/model/videoFile';
 import PlaylistFile from '../lib/model/playlistFile';
@@ -32,119 +32,131 @@ const Root = styled('div')(({ theme }) => {
 
 export default function HomePage() {
   const [currentTab, setCurrentTab] = React.useState(0);
-
-  // TODO: REMOVE AFTER TESTING
   const setPlaylist = useSetAtom(playlistPlayerAtom);
-  {
-    var file : VideoFile = new VideoFile();
-    file.fileName = "Men's Fashion";
-    file.filePath = "C:/mens_fashion.mp4";
-    var playlistFile : PlaylistFile = new PlaylistFile();
-    playlistFile.file = file;
-    playlistFile.timeStart = 0;
-    playlistFile.timeEnd = 1410;
-    playlistFile.volume = 100;
-
-    var file1 : VideoFile = new VideoFile();
-    file1.fileName = "Doctor Who Season 3 Episode 1";
-    file1.filePath = "C:/Users/fang2/OneDrive/Documents/coding/RBMP/sample_video/doctor_who_3_1.mp4";
-    var playlistFile1 : PlaylistFile = new PlaylistFile();
-    playlistFile1.file = file1;
-    playlistFile1.timeStart = 1411;
-    playlistFile1.timeEnd = 1439;
-    playlistFile1.volume = 100;
-
-    var temp : Array<PlaylistFile> = new Array<PlaylistFile>();
-    temp.push(playlistFile);
-    temp.push(playlistFile1);
-    setPlaylist(temp);
-  }
-
-  // TODO REMOVE AFTER TESTING
   const setConfigs = useSetAtom(playlistConfigAtom);
-  var tempType : VideoType = new VideoType();
-  tempType.id = 0;
-  tempType.name = "TV";
-  {
-    var config : PlaylistConfig = new PlaylistConfig();
-    var slots : Array<PlaylistSlot> = new Array<PlaylistSlot>();
-    
-    var tempSlot = new PlaylistSlot();
-    tempSlot.startTime = 0;
-    tempSlot.endTime = 59;
-    tempSlot.type = tempType;
-    tempSlot.muted = false;
-    tempSlot.volume = 50;
-    slots.push(tempSlot);
-    
-    var tempSlot1 = new PlaylistSlot();
-    tempSlot1.startTime = 60;
-    tempSlot1.endTime = 119;
-    tempSlot1.type = tempType;
-    tempSlot1.muted = true;
-    tempSlot1.volume = 20;
-    slots.push(tempSlot1);
-
-    var tempSlot2 = new PlaylistSlot();
-    tempSlot2.startTime = 120;
-    tempSlot2.endTime = 719;
-    tempSlot2.type = tempType;
-    tempSlot2.muted = false;
-    tempSlot2.volume = 70;
-    slots.push(tempSlot2);
-
-    var tempSlot3 = new PlaylistSlot();
-    tempSlot3.startTime = 720;
-    tempSlot3.endTime = 889;
-    tempSlot3.type = tempType;
-    tempSlot3.muted = false;
-    tempSlot3.volume = 100;
-    slots.push(tempSlot3);
-
-    var tempSlot4 = new PlaylistSlot();
-    tempSlot4.startTime = 890;
-    tempSlot4.endTime = 1079;
-    tempSlot4.type = tempType;
-    tempSlot4.muted = false;
-    tempSlot4.volume = 0;
-    slots.push(tempSlot4);
-
-    var tempSlot5 = new PlaylistSlot();
-    tempSlot5.startTime = 1080;
-    tempSlot5.endTime = 1259;
-    tempSlot5.type = tempType;
-    tempSlot5.muted = true;
-    tempSlot5.volume = 100;
-    slots.push(tempSlot5);
-
-    var tempSlot6 = new PlaylistSlot();
-    tempSlot6.startTime = 1260;
-    tempSlot6.endTime = 1440;
-    tempSlot6.type = tempType;
-    tempSlot6.muted = false;
-    tempSlot6.volume = 10;
-    slots.push(tempSlot6);
-
-    config.slots = slots;
-    config.name = "Default";
-    config.id = 0;
-    config.start_time = 0;
-    config.end_time = 1440;
-    config.description = "A default configuration for testing purposes.";
-
-    var configs = new Array<PlaylistConfig>()
-    configs.push(config);
-    setConfigs(configs);
-  }
-
-  // TODO: Remove after testing
   const setFiles = useSetAtom(filesAtom);
-  {
-    var files : Array<VideoFile> = new Array<VideoFile>();
-    files.push(file);
-    files.push(file1);
-    setFiles(files);
+  const setTypes = useSetAtom(typesAtom);
+
+  function setup() {
+    console.log("Setting Atoms!!");
+    // TODO: REMOVE AFTER TESTING
+    {
+      var file : VideoFile = new VideoFile();
+      file.fileName = "Men's Fashion";
+      file.filePath = "C:/mens_fashion.mp4";
+      var playlistFile : PlaylistFile = new PlaylistFile();
+      playlistFile.file = file;
+      playlistFile.timeStart = 0;
+      playlistFile.timeEnd = 1410;
+      playlistFile.volume = 100;
+  
+      var file1 : VideoFile = new VideoFile();
+      file1.fileName = "Doctor Who Season 3 Episode 1";
+      file1.filePath = "C:/Users/fang2/OneDrive/Documents/coding/RBMP/sample_video/doctor_who_3_1.mp4";
+      var playlistFile1 : PlaylistFile = new PlaylistFile();
+      playlistFile1.file = file1;
+      playlistFile1.timeStart = 1411;
+      playlistFile1.timeEnd = 1439;
+      playlistFile1.volume = 100;
+  
+      var temp : Array<PlaylistFile> = new Array<PlaylistFile>();
+      temp.push(playlistFile);
+      temp.push(playlistFile1);
+      setPlaylist(temp);
+    }
+  
+    // TODO REMOVE AFTER TESTING
+    var tempType : VideoType = new VideoType();
+    tempType.id = 0;
+    tempType.name = "TV";
+    {
+      var config : PlaylistConfig = new PlaylistConfig();
+      var slots : Array<PlaylistSlot> = new Array<PlaylistSlot>();
+      
+      var tempSlot = new PlaylistSlot();
+      tempSlot.startTime = 0;
+      tempSlot.endTime = 59;
+      tempSlot.type = tempType;
+      tempSlot.muted = false;
+      tempSlot.volume = 50;
+      slots.push(tempSlot);
+      
+      var tempSlot1 = new PlaylistSlot();
+      tempSlot1.startTime = 60;
+      tempSlot1.endTime = 119;
+      tempSlot1.type = tempType;
+      tempSlot1.muted = true;
+      tempSlot1.volume = 20;
+      slots.push(tempSlot1);
+  
+      var tempSlot2 = new PlaylistSlot();
+      tempSlot2.startTime = 120;
+      tempSlot2.endTime = 719;
+      tempSlot2.type = tempType;
+      tempSlot2.muted = false;
+      tempSlot2.volume = 70;
+      slots.push(tempSlot2);
+  
+      var tempSlot3 = new PlaylistSlot();
+      tempSlot3.startTime = 720;
+      tempSlot3.endTime = 889;
+      tempSlot3.type = tempType;
+      tempSlot3.muted = false;
+      tempSlot3.volume = 100;
+      slots.push(tempSlot3);
+  
+      var tempSlot4 = new PlaylistSlot();
+      tempSlot4.startTime = 890;
+      tempSlot4.endTime = 1079;
+      tempSlot4.type = tempType;
+      tempSlot4.muted = false;
+      tempSlot4.volume = 0;
+      slots.push(tempSlot4);
+  
+      var tempSlot5 = new PlaylistSlot();
+      tempSlot5.startTime = 1080;
+      tempSlot5.endTime = 1259;
+      tempSlot5.type = tempType;
+      tempSlot5.muted = true;
+      tempSlot5.volume = 100;
+      slots.push(tempSlot5);
+  
+      var tempSlot6 = new PlaylistSlot();
+      tempSlot6.startTime = 1260;
+      tempSlot6.endTime = 1439;
+      tempSlot6.type = tempType;
+      tempSlot6.muted = false;
+      tempSlot6.volume = 10;
+      slots.push(tempSlot6);
+  
+      config.slots = slots;
+      config.name = "Default";
+      config.id = 0;
+      config.start_time = 0;
+      config.end_time = 1439;
+      config.description = "A default configuration for testing purposes.";
+  
+      var configs = new Array<PlaylistConfig>()
+      configs.push(config);
+      setConfigs(configs);
+    }
+  
+    // TODO: Remove after testing
+    {
+      var files : Array<VideoFile> = new Array<VideoFile>();
+      files.push(file);
+      files.push(file1);
+      setFiles(files);
+    }
+  
+    // TODO: Remove after Testing
+    {
+      var types : Array<VideoType> = new Array<VideoType>();
+      types.push(tempType);
+      setTypes(types);
+    }
   }
+  
 
   const handleTabChange = (event: React.SyntheticEvent, newTab: number) => {
     setCurrentTab(newTab);
@@ -153,6 +165,7 @@ export default function HomePage() {
   const [hasWindow, setHasWindow] = useState(false);
   useEffect(() => {
     console.log("HOME REFRESH");
+    setup();
     if (typeof window !== "undefined") {
       setHasWindow(true);
     }
